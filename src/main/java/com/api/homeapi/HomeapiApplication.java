a6c3ab24924e309517a4ec55a0ad1a3cbd9fee0b
Jake Todd <jaketodd819@gmail.com>
2020-09-05 12:35:00 -0500

    init api

---
+++ src/main/java/com/api/homeapi/HomeapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.homeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeapiApplication.class, args);
+	}
+
+}
