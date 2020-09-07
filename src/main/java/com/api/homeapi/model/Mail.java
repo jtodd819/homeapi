ec2ed862db32ea86f62d1c92b1fcb610d9c3e8ba
Jake Todd <jaketodd819@gmail.com>
2020-09-06 20:17:25 -0500

    add mail sender

--- src/main/java/com/api/homeapi/model/Mail.java
+++ src/main/java/com/api/homeapi/model/Mail.java
@@ -2,23 +2,11 @@ package com.api.homeapi.model;
 
 public class Mail {
     private String from;
-    private String to;
     private String subject;
     private String body;
 
-    public Mail(String from, String to, String subject, String body) {
-        this.from = from;
-        this.to = to;
-        this.subject = subject;
-        this.body = body;
-    }
-
     public String getFrom() {
         return from;
-    }
-
-    public String getTo() {
-        return to;
     }
 
     public String getSubject() {
